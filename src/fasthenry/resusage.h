845e48e355b0be9675252fe392fbde45ce87f172
samerps <68449711+samerps@users.noreply.github.com>
2024-10-02 18:26:15 +0100

    disabled further timers

--- src/fasthenry/resusage.h
+++ src/fasthenry/resusage.h
@@ -25,28 +25,28 @@ static long sectime, utime;
 #ifdef NOTOTHER
 
 #ifdef FOUR			/* 4.2,3BSD (tested: Sun4, IBM6000, DEC5000) */
-#define starttimer getrusage(RUSAGE_SELF, &timestuff); \
-sectime = timestuff.ru_utime.tv_sec; \
-utime = timestuff.ru_utime.tv_usec
-#define stoptimer getrusage(RUSAGE_SELF, &timestuff); \
-dtime = (double)(timestuff.ru_utime.tv_sec - sectime) \
-        + 1.0e-6*(double)(timestuff.ru_utime.tv_usec - utime)
+// #define starttimer getrusage(RUSAGE_SELF, &timestuff); \
+// sectime = timestuff.ru_utime.tv_sec; \
+// utime = timestuff.ru_utime.tv_usec
+// #define stoptimer getrusage(RUSAGE_SELF, &timestuff); \
+// dtime = (double)(timestuff.ru_utime.tv_sec - sectime) \
+//         + 1.0e-6*(double)(timestuff.ru_utime.tv_usec - utime)
 #define DUMPRSS			/*  */
 #endif /* FOUR */
 
 #ifdef FIVE			/* for System V (tested: HP300) */
-#define starttimer times(&timestuff); \
-utime = timestuff.tms_utime
-#define stoptimer times(&timestuff); \
-dtime = (timestuff.tms_utime)-utime; \
-dtime /= HZ
+// #define starttimer times(&timestuff); \
+// utime = timestuff.tms_utime
+// #define stoptimer times(&timestuff); \
+// dtime = (timestuff.tms_utime)-utime; \
+// dtime /= HZ
 #define DUMPRSS			/*  */
 #endif /* FIVE */
 
 #else				/* default - no timers */
 
-#define starttimer		/*  */
-#define stoptimer		/*  */
+// #define starttimer		/*  */
+// #define stoptimer		/*  */
 #define DUMPRSS			/*  */
 
 #endif /* NOTOTHER */
